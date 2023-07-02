ba49f93c9e4701143ea183aa521cb52b3e3c7254
ibqn <evgen.ibqn@gmail.com>
2023-07-02 22:17:36 +0200

    add draft of subscribe api route

---
+++ src/app/api/subscribe/route.ts
@@ -0,0 +1,20 @@
+import { prisma } from '@/lib/prisma'
+import { NextResponse } from 'next/server'
+
+type Body = {
+  email: string
+}
+
+export async function POST(request: Request) {
+  const body = (await request.json()) as Body
+
+  const { email } = body
+
+  const subscription = await prisma.subscription.create({
+    data: {
+      email,
+    },
+  })
+
+  return NextResponse.json(subscription)
+}
