2c0e1fbcc9ecf17a3849a42bff8e9d112e52b9e9
ibqn <evgen.ibqn@gmail.com>
2023-07-04 22:11:31 +0200

    add react email

---
+++ emails/index.tsx
@@ -0,0 +1,12 @@
+import { Html, Button } from '@react-email/components'
+import * as React from 'react'
+
+export default function Email() {
+  return (
+    <Html>
+      <Button pX={20} pY={12} href="https://example.com" style={{ background: '#000', color: '#fff' }}>
+        Click me
+      </Button>
+    </Html>
+  )
+}
