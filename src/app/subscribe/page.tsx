d810cf0dfef1a17aaf48c26bca53417afe815d1e
ibqn <evgen.ibqn@gmail.com>
2023-07-09 19:53:14 +0200

    prettify

--- src/app/subscribe/page.tsx
+++ src/app/subscribe/page.tsx
@@ -1,5 +1,5 @@
-import { Logo } from '@/components/logo'
-import { SubscribeForm } from '@/components/subscribe-form'
+import { Logo } from "@/components/logo"
+import { SubscribeForm } from "@/components/subscribe-form"
 
 type Props = {}
 
