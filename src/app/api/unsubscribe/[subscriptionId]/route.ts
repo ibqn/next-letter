9f013ba6d45e84893f50fa4979b0372101a015a1
ibqn <evgen.ibqn@gmail.com>
2023-07-16 22:28:17 +0200

    working on unsubscribe route

---
+++ src/app/api/unsubscribe/[subscriptionId]/route.ts
@@ -0,0 +1,22 @@
+import { prisma } from "@/lib/prisma"
+import { NextResponse } from "next/server"
+
+type Props = {
+  params: {
+    subscriptionId: string
+  }
+}
+
+export async function DELETE(request: Request, { params }: Props) {
+  const { subscriptionId } = params
+
+  try {
+    const subscription = await prisma.subscription.delete({
+      where: { id: subscriptionId },
+    })
+
+    return NextResponse.json(subscription)
+  } catch (error) {
+    return NextResponse.json({ message: "error occurred on un-subscription" }, { status: 500 })
+  }
+}
