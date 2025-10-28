240fc7c89eb7bde3cff1ca2acb278a633c5bc6d9
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-10-28 10:21:22 +0100

    refactor: reorganize Card component structure and improve layout

--- packages/frontend/src/app/page.tsx
+++ packages/frontend/src/app/page.tsx
@@ -1,11 +1,10 @@
 import { Logo } from "@/components/logo"
 import { SubscribeForm } from "@/components/subscribe-form"
-import { Card, CardContent, CardHeader } from "@/components/ui/card"
-
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 
 export default function Subscribe() {
   return (
-    <main className="container flex flex-col p-0 gap-20 md:py-24">
+    <main className="container flex flex-col gap-20 p-0 md:py-24">
       <div className="mx-auto flex flex-col rounded-lg border border-neutral-400/40 px-10 py-8 shadow-xl md:w-1/2">
         <div className="flex flex-col items-center justify-center gap-4 sm:flex-row">
           <Logo className="h-20 w-20" />
@@ -23,24 +22,24 @@ export default function Subscribe() {
       </div>
 
       <Card className="mx-auto w-full max-w-lg">
-        <CardHeader className="flex gap-6">
-          <Logo className="size-20" />
-          <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight md:text-left lg:text-5xl">
-            Join my Newsletter!
-          </h1>
-          </CardHeader>
+        <CardHeader>
+          <CardTitle className="flex gap-6">
+            <Logo className="size-20" />
+            <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight md:text-left lg:text-5xl">
+              Join my Newsletter!
+            </h1>
+          </CardTitle>
+        </CardHeader>
         <CardContent>
-        <div className="flex flex-col items-center justify-center gap-4 sm:flex-row">
-
-        </div>
-        <p className="leading-7 not-first:mt-6">
-          Elevate your coding skills with exclusive access to the latest tutorials, expert tips, coding challenges, and
-          more! Whether you are a beginner or a seasoned developer, my compact newsletter has everything to keep you
-          ahead. Join the community and transform your coding dreams into reality. Sign up for free today!
-        </p>
-        <SubscribeForm />
-        <p className="mt-4">I respect your privacy. Unsubscribe at any time.</p>
-      </CardContent>
+          <div className="flex flex-col items-center justify-center gap-4 sm:flex-row"></div>
+          <p className="leading-7 not-first:mt-6">
+            Elevate your coding skills with exclusive access to the latest tutorials, expert tips, coding challenges,
+            and more! Whether you are a beginner or a seasoned developer, my compact newsletter has everything to keep
+            you ahead. Join the community and transform your coding dreams into reality. Sign up for free today!
+          </p>
+          <SubscribeForm />
+          <p className="mt-4">I respect your privacy. Unsubscribe at any time.</p>
+        </CardContent>
       </Card>
     </main>
   )
