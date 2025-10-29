883fa551c1ea9308880a06b9bc5117e82faeda7c
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-10-29 16:50:36 +0100

    feat: integrate reCAPTCHA verification into subscription creation handler

--- packages/backend/src/routes/subscription-post-route.ts
+++ packages/backend/src/routes/subscription-post-route.ts
@@ -4,11 +4,14 @@ import { SubscriptionClient } from "database/src/clients/subscription-client"
 import { createFactory } from "hono/factory"
 import { LoggerFactory } from "shared/src/utils/logger"
 import { createSubscriptionSchema } from "database/src/validators/subscription"
-import { response, type SuccessResponse } from "shared/src/response"
+import { error, response, type ErrorResponse, type SuccessResponse } from "shared/src/response"
 import type { Subscription } from "database/src/drizzle/schema/subscription"
+import { ReCaptchaClient } from "../clients/recaptcha-client"
+import { reCaptchaTokenSchema } from "shared/src/validators/rechaptcha"
 
 export type SubscriptionPostRouteConfig = {
   subscriptionClient: SubscriptionClient
+  reCaptchaClient: ReCaptchaClient
 }
 
 export class SubscriptionPostRoute {
@@ -17,21 +20,34 @@ export class SubscriptionPostRoute {
 
   constructor(private config: SubscriptionPostRouteConfig) {}
 
-  public handler = this.factory.createHandlers(zValidator("json", createSubscriptionSchema), async (c) => {
-    this.logger.info("Handling subscription creation")
+  public handler = this.factory.createHandlers(
+    zValidator("json", createSubscriptionSchema.and(reCaptchaTokenSchema)),
+    async (c) => {
+      this.logger.info("Handling subscription creation")
 
-    const subscriptionData = c.req.valid("json")
+      const { token, ...subscriptionData } = c.req.valid("json")
 
-    const subscription = await this.config.subscriptionClient.createSubscriptionItem(subscriptionData)
+      this.logger.info("Verifying reCAPTCHA token")
+      const reCaptchaResult = await this.config.reCaptchaClient.verifyToken(token)
+      this.logger.info({ reCaptchaResult }, "reCAPTCHA verification result")
 
-    this.logger.info({ subscriptionId: subscription.id }, "Successfully created subscription")
+      if (!reCaptchaResult.success) {
+        this.logger.warn("reCAPTCHA verification failed")
+        return c.json<ErrorResponse>(error("reCAPTCHA verification failed"), 403)
+      }
 
-    return c.json<SuccessResponse<Subscription>>(response(subscription), 201)
-  })
+      const subscription = await this.config.subscriptionClient.createSubscriptionItem(subscriptionData)
+
+      this.logger.info({ subscriptionId: subscription.id }, "Successfully created subscription")
+
+      return c.json<SuccessResponse<Subscription>>(response(subscription), 201)
+    }
+  )
 }
 
 const subscriptionPostDependencies: SubscriptionPostRouteConfig = {
   subscriptionClient: new SubscriptionClient(),
+  reCaptchaClient: new ReCaptchaClient(),
 }
 
 export const subscriptionPostRoute = () => new SubscriptionPostRoute(subscriptionPostDependencies).handler
